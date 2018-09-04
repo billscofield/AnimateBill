6f8d2c91c8585798ccc65b325b9ec129566f5a6c
billscofield <billscofield@hotmail.com>
2018-09-04 09:00:46 +0800

    it's time to make a css animation libary myself

---
+++ AnimateBill.js
@@ -0,0 +1,45 @@
+@keyframes fadein{
+    0%{opacity:0;}
+    100%{opacity:1;}
+}
+@keyframes fadeout{
+    0%{opacity:1;}
+    100%{opacity:0;}
+}
+@keyframes fadeInFromTop{
+    0%{opacity:0;transform:translateY(-100px);}
+    100%{opacity:1;transform:translateY(0)}
+}
+@keyframes fadeInFromBottom{
+        0%{opacity:0;transform:translateY(100px);}
+        100%{opacity:1;transform:translateY(0)}
+}
+@keyframes fadeInFromLeft{
+        0%{opacity:0;transform:translateX(-100px);}
+        100%{opacity:1;transform:translateY(0)}
+}
+@keyframes fadeInFromRight{
+        0%{opacity:0;transform:translateX(100px);}
+        100%{opacity:1;transform:translateY(0)}
+}
+
+@keyframes fadeout{
+    0%{opacity:1;}
+    100%{opacity:0;}
+}
+@keyframes fadeOutFromTop{
+    0%{opacity:0;transform:translateY(0px);}
+    100%{opacity:1;transform:translateY(-100px)}
+}
+@keyframes fadeOutFromBottom{
+        0%{opacity:0;transform:translateY(0);}
+        100%{opacity:1;transform:translateY(100px)}
+}
+@keyframes fadeOutFromLeft{
+        0%{opacity:0;transform:translateX(0);}
+        100%{opacity:1;transform:translateY(-100px)}
+}
+@keyframes fadeOutFromRight{
+        0%{opacity:0;transform:translateX(0);}
+        100%{opacity:1;transform:translateY(100px)}
+}
